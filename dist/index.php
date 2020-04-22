bea12289f5fa0ab4a6ca12a4041e647327fb8b75
cilufarag <29257363+cilufarag@users.noreply.github.com>
2020-04-22 10:59:22 +0200

    changes to index.php

--- dist/index.php
+++ dist/index.php
@@ -11,12 +11,15 @@
     <?php include($meta); ?>
     
     <!-- Bootstrap CSS -->
+    <?php include($bootstrapCss); ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- CUSTOM FONTS -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/9ec45dcb51.js" crossorigin="anonymous"></script>
-
+    <!-- Google Fonts CSS -->
+    <?php include($googleApisCss); ?>
+    
+    <!-- Font Awesome Script -->
+    <?php include($fontAwesomeScript); ?>
+    
     <!-- CUSTOM CSS -->
     <link href="assets/css/styles.css" rel="stylesheet">
 
@@ -449,9 +452,10 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js " integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo " crossorigin="anonymous "></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js " integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1 " crossorigin="anonymous "></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js " integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM " crossorigin="anonymous "></script>
+    <?php include($bootstrapScripts); ?>
+    
+    <!-- Custom javaScript -->
+    <script src=""></script>
 </body>
 
 </html>
