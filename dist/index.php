f6403c36ec6c51baa20a49526d2634e5b8112393
cilufarag <29257363+cilufarag@users.noreply.github.com>
2020-04-22 10:28:10 +0200

    Created fontAwesome-script.inc.php

--- dist/index.php
+++ dist/index.php
@@ -4,14 +4,14 @@
 <html lang="en">
 
 <head>
+    <!-- Page title -->
+    <title>Smart Solutions Home Page</title>
+
     <!-- Required meta tags -->
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <?php include($meta); ?>
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
 
     <!-- CUSTOM FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
@@ -20,7 +20,6 @@
     <!-- CUSTOM CSS -->
     <link href="assets/css/styles.css" rel="stylesheet">
 
-    <title>Smart Solutions Home Page</title>
 </head>
 
 <body>
