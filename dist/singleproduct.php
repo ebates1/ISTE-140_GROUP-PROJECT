b921905938cd20af7c064c47603e8b9038c01b93
cilufarag <29257363+cilufarag@users.noreply.github.com>
2020-04-22 11:42:43 +0200

    Created all html pages to php pages

---
+++ dist/singleproduct.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <!-- CUSTOM FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9ec45dcb51.js" crossorigin="anonymous"></script>
+
+    <!-- CUSTOM CSS -->
+    <link href="assets/css/styles.css" rel="stylesheet">
+
+    <title>Single Product</title>
+</head>
+
+<body>
+
+    <!-- MAIN HEADER NAVBAR -->
+
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top light-blue">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+            <ul class="navbar-nav">
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+
+                <li class="nav-item px-2 dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">FAQ</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown08">
+                        <a class="dropdown-item" href="#">Shipping</a>
+                        <a class="dropdown-item" href="#">Return</a>
+                        <a class="dropdown-item" href="#">Delivery</a>
+                        <a class="dropdown-item" href="#">Payment</a>
+                    </div>
+                </li>
+
+
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="cart.html">Shopping Cart
+                    <i class="pl-3 fas fa-shopping-cart"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <!-- END OF MAIN HEADER NAVBAR -->
+
+    <!-- MAIN PRODUCT INFO -->
+
+    <div class="container my-5">
+        <div class="row align-items-center ">
+            <div class="col-lg-6 p-5">
+                <img src="assets/img/amazon-alexa.png" class="img-fluid" alt="Responsive image">
+            </div>
+
+            <div class="col-lg-6 d-flex flex-column align-items-start">
+                <h1 class="mb-4">Moto Smart Speaker with Amazon Alexa</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero odio, nostrum doloremque ullam iste sequi perspiciatis voluptate commodi natus eum laborum provident cupiditate asperiores est sit quibusdam assumenda? Iusto, asperiores!</p>
+
+                <h4><strong>$100</strong></h4>
+
+                <!-- COLOR OPTIONS-->
+                <h6>Color</h6>
+
+
+                <div class="custom-radios d-flex">
+                    <div>
+                        <input type="radio" id="color-1" name="color" value="color-1" checked>
+                        <label for="color-1">
+                      <span>
+                      </span>
+                    </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" id="color-2" name="color" value="color-2">
+                        <label for="color-2">
+                      <span>
+                      </span>
+                    </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" id="color-3" name="color" value="color-3">
+                        <label for="color-3">
+                      <span>
+                      </span>
+                    </label>
+                    </div>
+
+                </div>
+
+
+                <h6 class="mt-2">Plug Configuration</h6>
+
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+
+                    <label class="btn btn-secondary active mr-3 px-4 rounded active">
+                      <input type="radio" name="options" id="option1">UK
+                    </label>
+
+                    <label class="btn btn-secondary active mr-3 px-4 rounded">
+                        <input type="radio" name="options" id="option2">US
+                      </label>
+
+
+                </div>
+
+                <a href="# " class="btn btn-primary px-4 mt-4">Add to cart</a>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <!-- END OF MAIN PRODUCT INFO -->
+
+
+    <!-- FOOTER -->
+
+    <footer class="page-footer ">
+
+        <!-- Footer Links -->
+        <div class="container text-center py-5 text-md-left ">
+            <!-- Grid row -->
+            <div class="row ">
+
+                <!-- Grid column -->
+                <div class="col-md-4 mx-auto pr-5 ">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis optio autem, quos exercitationem tempore, quo deleniti, perferendis provident ipsa hic excepturi dolorum modi laudantium. Voluptate dignissimos aliquid quam dicta quod!
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none ">
+
+                <!-- Grid column -->
+                <div class="col-md-2 mx-auto ">
+
+                    <!-- Links -->
+
+                    <ul class="list-unstyled ">
+                        <li>
+                            <a href="#! ">About</a>
+                        </li>
+                        <li>
+                            <a href="#! ">Store</a>
+                        </li>
+                        <li>
+                            <a href="#! ">Blog</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none ">
+
+                <!-- Grid column -->
+                <div class="col-md-2 mx-auto ">
+
+                    <!-- Links -->
+
+                    <ul class="list-unstyled ">
+                        <li>
+                            <a href="#! ">Payment</a>
+                        </li>
+                        <li>
+                            <a href="#! ">Delivery</a>
+                        </li>
+                        <li>
+                            <a href="#! ">Returns</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none ">
+
+                <!-- Grid column -->
+                <div class="col-md-2 mx-auto ">
+
+                    <!-- Links -->
+
+                    <ul class="list-unstyled ">
+                        <li>
+                            <a href="#! ">Contact</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 mx-auto ">
+
+                    <!-- Social Links -->
+                    <h6>Follow us</h6>
+                    <div class="social-media-links ">
+                        <div>
+                            <a href="# ">
+                                <i class=" fab fa-facebook-f "></i>
+                            </a>
+                        </div>
+
+                        <div>
+                            <a href="# ">
+                                <i class="fab fa-instagram "></i>
+                            </a>
+                        </div>
+
+                        <div>
+                            <a href="# ">
+                                <i class="fab fa-twitter "></i> </a>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Grid column -->
+
+
+            </div>
+            <!-- Grid row -->
+
+
+            <!-- Copyright -->
+            <div class="py-3 border-top mt-4 ">© 2020 Inertial Media for RIT Croatia
+            </div>
+            <!-- Copyright -->
+
+        </div>
+
+
+    </footer>
+
+    <!-- Footer -->
+
+    <!-- END OF FOOTER-->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js " integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo " crossorigin="anonymous "></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js " integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1 " crossorigin="anonymous "></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js " integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM " crossorigin="anonymous "></script>
+
+</body>
+
+</html>
